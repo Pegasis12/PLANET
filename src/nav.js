dbb9362684d601c91827d2228c4ebdb5db7924ac
Pegasis12 <plsantana246@gmail.com>
2025-06-02 14:24:36 -0300

    first commit

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,38 @@
-const mobileButton = document.getElementById('mobile-button');
-const mobileMenu = document.getElementById('mobile-menu');
+function mobileButton(){
+  const mobileButton = document.getElementById('mobile-button');
+  const mobileMenu = document.getElementById('nav');
+  
+  mobileButton.addEventListener('click', () => {
+    mobileButton.classList.toggle('bg-white/40');
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('flex');
+  });
+  
+  mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.add('hidden');
+    mobileMenu.classList.remove('flex');
+  });
+}
 
-mobileButton.addEventListener('click', () => {
-  mobileButton.classList.toggle('bg-white/40');
-  mobileMenu.classList.toggle('hidden');
-  mobileMenu.classList.toggle('flex');
-});
+mobileButton();
 
-mobileMenu.addEventListener('click', () => {
-  mobileMenu.classList.add('hidden');
-  mobileMenu.classList.remove('flex');
-});
+function navAtivo(){
+  const links = document.querySelectorAll("nav li a");
+
+  function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+  
+    if (url.includes(href)) {
+      const span = link.closest("li").querySelector("span");
+      if (span) {
+        span.classList.remove("hidden");
+        span.classList.add("block");
+      }
+    }
+  }
+  
+  links.forEach(ativarLink);
+}
+
+navAtivo();
